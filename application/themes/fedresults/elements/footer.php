4e778c0617c0c431d135ac55a7fe29ed88d9540a
unknown <superkelp@hotmail.com>
2016-08-06 23:46:17 -0400

    theme setup

---
+++ application/themes/fedresults/elements/footer.php
@@ -0,0 +1,12 @@
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+
+</div><!--END PAGEWRAP CLASS; see header_meta for opening-->
+
+<footer class="footer" role="contentinfo">
+  FEDRESULTS FOOTER
+</footer>
+
+<?php Loader::element('footer_required'); ?>
+
+</body>
+</html>
