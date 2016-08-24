989649ec82da7ce62286ee8125ae4169c8675056
unknown <superkelp@hotmail.com>
2016-08-23 21:33:25 -0400

    header, footer, template tweaks

--- application/themes/fedresults/elements/footer.php
+++ application/themes/fedresults/elements/footer.php
@@ -1,7 +1,5 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-</div><!--END PAGEWRAP CLASS; see header_meta for opening-->
-
 <footer class="footer" role="contentinfo">
   <div class="container">
     <div class="row">
@@ -14,11 +12,17 @@
         </div>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <div class="footer__right">&copy;<?php echo date('Y');?> FedResults. All Rights Reserved.</div>
+        <div class="footer__right">
+          <p>&copy;<?php echo date('Y');?> FedResults. All Rights Reserved.
+            <a href="https://www.facebook.com/fedresults/" class="btn btn--primary btn--social" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="https://twitter.com/fedresults" class="btn btn--primary btn--social" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></p>          
+        </div>
       </div>
     </div>
   </div>
 </footer>
+
+</div><!--END PAGEWRAP CLASS; see header_meta for opening-->
 
 <?php Loader::element('footer_required'); ?>
 
--- application/themes/fedresults/elements/header.php
+++ application/themes/fedresults/elements/header.php
@@ -7,22 +7,29 @@ $displayThirdColumn = $blocks > 0 || $c->isEditMode();
 <div class="header-search" role="search">
     <div class="container">
         <div class="row">
-          <?php if ($displayThirdColumn) { ?>
-              <div class="col-xs-12 col-sm-4 col-sm-offset-8"><?php $as->display(); ?></div>
-          <?php } ?>
+            <div class="col-xs-12 col-sm-6 col-sm-offset-6">
+                <div class="header-search__group">
+                    <div class="header-search__text"><p><a href="mailto:sales@fedresults.com">sales@fedresults.com</a> <a href="tel:703-889-8700">703-889-8700</a></p></div>
+                    <div class="header-search__nav"><?php $as->display(); ?></div>
+                    <div class="header-search__icons">
+                      <a href="https://www.facebook.com/fedresults/" class="btn btn--primary btn--social" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                      <a href="https://twitter.com/fedresults" class="btn btn--primary btn--social" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 <header class="header-nav" role="banner">
     <div class="container">
         <div class="row">
-            <div class="col-xs-4 col-sm-3">
+            <div class="col-xs-12 col-sm-3">
                 <?php
                 $a = new GlobalArea('Header Site Logo');
                 $a->display();
                 ?>
             </div>
-            <div class="col-xs-8 col-sm-9" role="navigation">
+            <div class="col-xs-12 col-sm-9" role="navigation">
                 <?php
                 $a = new GlobalArea('Header Navigation');
                 $a->display();
