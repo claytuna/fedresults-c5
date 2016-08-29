dcc9734ba6c8fc7723caccdcf0823a0cd355345d
unknown <superkelp@hotmail.com>
2016-08-29 00:32:22 -0400

    events-slider update

--- application/blocks/fed_upcoming_events_slider/view.php
+++ application/blocks/fed_upcoming_events_slider/view.php
@@ -30,7 +30,7 @@ $(document).ready(function(){
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <div class="fed-events-slider__title">
+        <div class="fed-events-slider__main-title">
           <h2 class="title-underline"><?php echo $sliderTitle?></h2>
         </div>
 
@@ -41,16 +41,18 @@ $(document).ready(function(){
 
                   <?php if(count($rows) > 0) { ?>
                   <ul class="rslides" id="ccm-image-slider-<?php echo $bID ?>">
-                      <?php foreach($rows as $row) { ?>
+                      <?php foreach($rows as $key=>$row) { ?>
                           <li>
-                            <?php print_r($row)?>
-                            <?php print_r($row['linkPage']->getCollectionName()) ?>
-                            <?php print_r($row['linkPage']->getCollectionDatePublic()) ?>
-                            <?php print_r($row['linkPage']->getCollectionDescription()) ?>
+                            <div class="fed-events-slider__slide">
+                            <?php
+                            $page = $row['linkPage'];
+                            $url = $row['linkURL'];
+                             /*print_r(count($rows))
+                             print_r($key)*/?>
                             <?php $thumbnail = $row['linkPage']->getAttribute('thumbnail'); ?>
 
                             <?php if (is_object($thumbnail)): ?>
-                                <a href="<?php echo $row['linkURL'] ?>" class="fed-blog-entry__thumbnail" target="<?php echo $target ?>">
+                                <a href="<?php echo $url ?>" class="fed-events-slider__thumbnail" target="<?php echo $target ?>">
                                     <?php
                                     $img = Core::make('html/image', array($thumbnail));
                                     $tag = $img->getTag();
@@ -59,6 +61,49 @@ $(document).ready(function(){
                                     ?>
                                 </a>
                             <?php endif; ?>
+
+                            <div class="fed-events-slider__text">
+                            <?php
+                            $collectionDate = strtotime($page->getCollectionDatePublic(), true);
+                            $date = date("d/m/Y", $collectionDate);
+                            $dateDay = date("d", $collectionDate);
+                            $dateMonth = date("M", $collectionDate);
+                            $dateYear = date("Y", $collectionDate);
+
+                            if (isset($collectionDate) && $collectionDate): ?>
+                            <div class="fed-events-slider__date">
+                              <span class="fed-events-slider__date-month"><?php echo $dateMonth?></span>
+                              <span class="fed-events-slider__date-day"><?php echo $dateDay?></span>
+                              <span class="fed-events-slider__date-year"><?php echo $dateYear?></span>
+                            </div>
+                            <?php endif; ?>
+
+                            <div class="fed-events-slider__content">
+                            <?php
+                            $pageName = $page->getCollectionName();
+                            if (isset($pageName) && $pageName): ?>
+                                <a class="fed-events-slider__title" href="<?php echo $url ?>">
+                                  <?php echo $pageName ?>
+                                </a>
+                            <?php endif; ?>
+
+                            <?php
+                            $pageDesc = $page->getCollectionDescription();
+                            if (isset($pageDesc) && $pageDesc): ?>
+                                <div class="fed-events-slider__desc">
+                                    <?php echo $pageDesc ?>
+                                </div>
+                            <?php endif; ?>
+
+                                <div class="fed-events-slider__read-more">
+                                    <a href="<?php echo $url?>">
+                                      <?php echo t("Read more")?> <span class="fed-events-slider__read-more-arrows">>></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            </div>
+                          </div>
 
                           <?php if($row['linkURL']) { ?>
                               <a href="<?php echo $row['linkURL'] ?>" class="mega-link-overlay"></a>
